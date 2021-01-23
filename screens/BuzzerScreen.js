81b9cd4babf608540b9f013d3e1075efcdf50100
ailishnagpal <71159297+ailishnagpal@users.noreply.github.com>
2021-01-23 11:56:16 -0700

    Add files via upload

---
+++ screens/BuzzerScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import SoundButton from '../components/SoundButton'
+import AppHeader from '../components/AppHeader'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,45 @@
+import * as React from 'react'; 
+import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
+import AppHeader from '../components/AppHeader' 
+export default class HomeScreen extends React.Component {
+   goToBuzzerScreen=(buzzerColor)=> { 
+     this.props.navigation.navigate('BuzzerScreen',{color:buzzerColor}) 
+     } 
+     render(){ 
+       return( 
+         <View> 
+         <AppHeader/> 
+
+         <TouchableOpacity style={[styles.button,{backgroundColor:"red"}]} 
+         onPress={()=>this.goToBuzzerScreen("red")}>
+         <Text style={styles.buttonText}>Team 1</Text> 
+         </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.button,{backgroundColor:"blue"}]} 
+         onPress={()=>this.goToBuzzerScreen("blue")}> 
+         <Text style={styles.buttonText}>Team 2</Text> 
+         </TouchableOpacity>
+
+         <TouchableOpacity style={[styles.button,{backgroundColor:"green"}]} 
+         onPress={()=>this.goToBuzzerScreen("green")}> 
+         <Text style={styles.buttonText}>Team 3</Text> 
+         </TouchableOpacity>
+
+         <TouchableOpacity style={[styles.button,{backgroundColor:"yellow"}]} 
+         onPress={()=>this.goToBuzzerScreen("yellow")}> 
+         <Text style={styles.buttonText}>Team 4</Text> 
+         </TouchableOpacity>
+
+          </View> 
+          ) } } 
+          const styles = StyleSheet.create({ 
+            button:{ justifyContent : 'center',
+             alignSelf : 'center',
+              borderWidth : 2,
+              borderRadius : 15,
+              marginTop:150,
+              width : 200, 
+              height:50, 
+              backgroundColor:'green' }, 
+              buttonText : { textAlign : 'center', color : 'white' } 
+              })
